0823777e13251fd2c9f6a909c49b73d97d6d3b70
Troy <tprebenda@gmail.com>
2020-05-22 14:21:26 -0400

    5th commit

--- challenge/maps.js
+++ challenge/maps.js
@@ -1,8 +1,52 @@
+
+/// Default Values for heatmap
+
+var googlefoodData = {max: 0, data: []};
+var googlecommunityData = {max: 0, data: []};
+var googlebigshopsData = {max: 0, data: []};
+var googlesmallshopsData = {max: 0, data: []};
+var googletransitData = {max: 0, data: []};
+var googletouristData = {max: 0, data: []};
+
+var yelpfoodData = {max: 0, data: []};
+var yelpshoppingData = {max: 0, data: []};
+var yelpcommunityData= {max: 0, data: []};
+
+var walkscoreData = {max: 0, data: []};
+var averageData = {max: 0, data: []};
+var compositeData = {max: 0, data: []};
+var markercontainer = new Array();
+
+var layersactive = [];
+
+
+//// Google Reverse Geocodeing Setup (for address display) ////
+var geocoder = new google.maps.Geocoder;
+
+function geocodeLatLng(geocoder, lat, lng) {
+    var latlng = {lat: lat, lng: lng};
+    geocoder.geocode({'location': latlng}, function(results, status) {
+        if (status === 'OK') {
+            if (results[1]) {
+                document.getElementById('address').value = results[0].formatted_address;
+            } 
+            else {
+                document.getElementById('address').value = 'No results found';
+            }
+        }
+        else {
+            document.getElementById('address').value = 'Geocoder failed due to: ' + status;
+        }
+  });
+}
+
+
 /*                                                                      *
  *                  SETTING UP LEAFLETS API                             *
  *                                                                      */
 
-var mymap = L.map('map').setView([42.412660,-71.123060], 13);
+// Map is set around my house at Tufts University :)
+var mymap = L.map('map').setView([42.412660,-71.123060], 14);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -12,18 +56,149 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoidHByZWJlbmRhIiwiYSI6ImNrYWZ2MDQ1djAwaGMyeHF0OTV4eDVzenAifQ.lxCM4eHlgBPXKqeDIr8pFw'
 }).addTo(mymap);
 
+// var baseMaps = { "Map View": baseLayer };
 
-// Create marker
+mymap.on('click', onMapClick);
+
+/// Add Markers to Map on click, plus change scores when marker is dragged  ///
 var marker;
 function onMapClick(e) {
-    if (marker == null) {
-        marker = L.marker(e.latlng).addTo(mymap)
+    marker = L.marker(e.latlng, {draggable:'true'}).addTo(mymap);
+    marker.on('click', markerOnClick);
+    markercontainer.push(marker);
+  
+    for (var name in AllScores) {
+      document.getElementById(name).value = calculatescore(marker.getLatLng().lat,marker.getLatLng().lng, AllScores[name]);
+    }
+  
+    var currlocation = geocodeLatLng(geocoder, marker.getLatLng().lat, marker.getLatLng().lng);
+  
+    mymap.panTo(marker.getLatLng());
+  
+    marker.on('dragend', function(event) {
+        var marker = event.target;
+        var position = marker.getLatLng();
+        marker.setLatLng(new L.LatLng(position.lat, position.lng),{draggable:'true'});
+        currlocation = geocodeLatLng(geocoder, marker.getLatLng().lat, marker.getLatLng().lng);
+        marker.addTo(mymap)
+  
+        for (var name in AllScores) {
+            document.getElementById(name).value = escore(marker.getLatLng().lat,marker.getLatLng().lng, AllScores[name]);
+        }    
+        
+        var currlocation = geocodeLatLng(geocoder, marker.getLatLng().lat, marker.getLatLng().lng);
+  
+        mymap.panTo(new L.LatLng(position.lat, position.lng))
+  
+    });
+  
+    mymap.addLayer(marker);
+}
+
+
+/// Update scores on click ///
+function markerOnClick(e) {
+    for (var name in AllScores) {
+        document.getElementById(name).value = calculatescore(e.latlng.lat,e.latlng.lng, AllScores[name]); 
+    }
+    var currlocation = geocodeLatLng(geocoder, e.latlng.lat, e.latlng.lng);
+    mymap.panTo(new L.LatLng(e.latlng.lat, e.latlng.lng));
+}
+
+
+/********   HEATMAP SETUP:   **********/
+var cfg1 = {"radius": .007, "maxOpacity": .8, "scaleRadius": true, "useLocalExtrema": true, latField: 'lat', lngField: 'lng', valueField: 'count',  "blur": .8 };
+var compositelayer = new HeatmapOverlay(cfg1);  
+/// Set Data ///
+compositelayer.setData({max: 0, data:[]});
+/// Create Map Layers ///
+var compositegroup = L.layerGroup([compositelayer]);    
+/// Add Layers to Map ///
+mymap.addLayer(compositegroup);    
+
+
+
+/// Function for adding multiple heatmaps together ///
+function layertrigger(keyword){
+    index = layersactive.indexOf(keyword);
+    if (index > -1){
+        layersactive.splice(index, 1);
+    } 
+    else{
+        layersactive.push(keyword);
+    }
+    if (layersactive.length > 0) {
+        var newdata = clone(AllScores[layersactive[0]].data);
+        for (var j = 1; j < layersactive.length; j++) {
+            var adder = clone(AllScores[layersactive[j]].data);
+            for (var i = 0; i < newdata.length; i++) {
+            newdata[i].count = newdata[i].count + adder[i].count;
+            }
+        }
+
+        compositelayer.setData({max:1000, data: newdata}); 
     }
     else {
-    marker.setLatLng(e.latlng)
+        compositelayer.setData({max: 0, data: []});
     }
 }
-mymap.on('click', onMapClick);
+
+
+
+// Show checkboxes for heatmap layers   **DISPLAY**
+function showCheckboxes() {
+    var checkboxes = document.getElementById("checkboxes");
+    if (!expanded) {
+        checkboxes.style.display = "block";
+        expanded = true;
+    } 
+    else {
+        checkboxes.style.display = "none";
+        expanded = false;
+    }
+}
+
+
+// Google Searchbox:
+var searchBox = new google.maps.places.SearchBox(document.getElementById('mapsearch'));
+google.maps.event.addListener(searchBox, 'places_changed', function() {
+    for (var i = 0; i < searchBox.getPlaces().length; i++) {
+        marker = L.marker([searchBox.getPlaces()[i].geometry.location.lat(), searchBox.getPlaces()[i].geometry.location.lng()], {draggable:'true'});
+        marker.on('click', markerOnClick);
+        markercontainer.push(marker);
+    
+        for (var name in AllScores){
+            document.getElementById(name).value = calculatescore(marker.getLatLng().lat,marker.getLatLng().lng, AllScores[name]); 
+        }
+
+        var currlocation = geocodeLatLng(geocoder, marker.getLatLng().lat, marker.getLatLng().lng);
+
+        marker.addTo(mymap)
+        mymap.panTo(marker.getLatLng());
+
+        marker.on('dragend', function(event) {
+            var marker = event.target;
+            var position = marker.getLatLng();
+            marker.setLatLng(new L.LatLng(position.lat, position.lng),{draggable:'true'});
+
+            for (var name in AllScores){
+                document.getElementById(name).value = calculatescore(marker.getLatLng().lat,marker.getLatLng().lng, AllScores[name]);
+            }    
+
+            document.getElementById('address').value = searchBox.getPlaces()[0].address;
+            mymap.panTo(new L.LatLng(position.lat, position.lng))
+
+        });
+        mymap.addLayer(marker);
+    }
+
+});
+
+
+// Rectangle:
+var rect = L.rectangle([[42.418277, -71.109133], [42.410443, -71.141051]], { dashArray: "10", color: "#4d4d4d",  opacity: .8,  fillOpacity: 0});
+mymap.addLayer(rect);
+
 
 
 
--- challenge/scores.js
+++ challenge/scores.js
@@ -1,15 +1,83 @@
-// Heatmap Checkboxes
 
-var expanded = false;
-function showCheckboxes() {
-  var checkboxes = document.getElementById("checkboxes");
-  if (!expanded) {
-    checkboxes.style.display = "block";
-    expanded = true;
-  } else {
-    checkboxes.style.display = "none";
-    expanded = false;
-  }
+/// Function to clone a javascript object ///
+function clone(obj) {
+    // Handle the 3 simple types, and null or undefined
+    if (null == obj || "object" != typeof obj) {
+        return obj;
+    }
+
+    // Handle Date
+    if (obj instanceof Date) {
+        var copy = new Date();
+        copy.setTime(obj.getTime());
+        return copy;
+    }
+
+    // Handle Array
+    if (obj instanceof Array) {
+        var copy = [];
+        for (var i = 0, len = obj.length; i < len; i++) {
+            copy[i] = clone(obj[i]);
+        }
+        return copy;
+    }
+
+    // Handle Object
+    if (obj instanceof Object) {
+        var copy = {};
+        for (var attr in obj) {
+            if (obj.hasOwnProperty(attr)) {
+                copy[attr] = clone(obj[attr]);
+            }
+        }
+        return copy;
+    }
+
+    throw new Error("Unable to copy obj! Its type isn't supported.");
 }
 
 
+
+/***********    SCORING SECTION     ************/
+
+/// Get Scores of Closest Location ///
+function closestscore (lat, lng, arr) {
+    var bestdist = Math.pow(Math.abs(lat - arr[0].lat), 2) + Math.pow(Math.abs(lng - arr[0].lng), 2);
+    var bestindex = 0;
+
+    for (i = 1; i < arr.length; i++){
+        // currlat = arr[i].lat;
+        // currlng = arr[i].lng;
+        currdist = Math.pow(Math.abs(lat - arr[i].lat), 2) + Math.pow(Math.abs(lng - arr[i].lng), 2);
+        if (currdist < bestdist) {
+            // bestlat = currlat;
+            // bestlng = currlng;
+            bestindex = i;
+            bestdist = currdist;
+        }
+    }
+
+    return arr[bestindex].count
+}
+
+/// Return scores between 0 and 10 ///
+
+function calculatescore(lat, lng, heatmapdata){
+    score = closestscore(lat, lng, heatmapdata.data);
+    return Math.round(1000*score/heatmapdata.max)/100; // Returns two decimal places
+}
+
+
+/// Code for showing all scores ///
+var button = document.getElementById('showscores');
+var shown = false;
+button.onclick = function() { 
+  if (shown){
+      document.getElementById('allscores').style.visibility='hidden';  
+      shown = !shown; 
+  }
+  else{
+      document.getElementById('allscores').style.visibility='visible';  
+      shown = !shown;
+  }
+}

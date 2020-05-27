38567fc8c5d09f26466a7ddfdbbd5c8991555be1
Troy <tprebenda@gmail.com>
2020-05-27 14:04:49 -0400

    Prototype complete

--- challenge/mapping.js
+++ challenge/mapping.js
@@ -316,8 +316,8 @@ document.getElementById('selectCity').onchange = function() {
     var myScript = document.createElement('script');
     var file = (this.value.split(",")[0] != 'Cambridge') ? this.value.split(",")[0] : 'Default';
 
-    // To run on local server, change '/challenge/data/' back to '/data/'
-    myScript.setAttribute('src', '/data/' + file + 'Heatdata.js');
+    // To run on local server, change 'challenge/data/' back to '/data/'
+    myScript.setAttribute('src', 'challenge/data/' + file + 'Heatdata.js');
     document.head.appendChild(myScript);
 
     myScript.onload = function() {
